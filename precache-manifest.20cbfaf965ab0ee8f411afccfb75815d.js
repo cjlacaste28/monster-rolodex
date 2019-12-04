17c17b9291ddabe0870f9c3bc5db677236976cdb
Christian Lacaste <cjlacaste28@gmail.com>
2019-12-04 15:34:38 +0800

    Updates

---
+++ precache-manifest.20cbfaf965ab0ee8f411afccfb75815d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a743580598a4a64f75f9c32ef983b2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0dc48826168ddf7ac0e2",
+    "url": "/monster-rolodex/static/css/main.976f639f.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monster-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "0dc48826168ddf7ac0e2",
+    "url": "/monster-rolodex/static/js/main.f8fdb13c.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  }
+]);
